8f28da172480e0ee5b64312feb8172d1bcfc1eb6
Novang <qkdxorjs1002@naver.com>
2021-06-07 01:30:04 +0900

    VerifierController: Hopae

--- src/main/java/kr/hopae/core/controller/VerifierController.java
+++ src/main/java/kr/hopae/core/controller/VerifierController.java
@@ -17,53 +17,58 @@ public class VerifierController {
     @Resource(name = "ariesRepository")
     private AriesRepository ariesRepository;
 
-    @RequestMapping(value = "/knock", method = RequestMethod.GET)
-    public Map<String, Object> knock(@RequestParam String alias) throws Exception {
+    @RequestMapping(value = "/prove", method = RequestMethod.GET)
+    public Map<String, Object> requestProof(@RequestParam String alias) throws Exception {
 
         // Get Connections
         ConnectionList connectionList = ariesRepository.getConnections(alias);
         ConnRecord connRecord = connectionList.results.get(0);
 
         // Get Credential Definitions
-        CredentialDefinitionsCreatedResult creDefCreatedResult = ariesRepository.getCredentialDefs("account_schema");
-        String creDefId = creDefCreatedResult.credentialDefinitionIds.get(0);
+        CredentialDefinitionsCreatedResult creDefCreatedResult = ariesRepository.getCredentialDefs("hopae_pass");
+        String creDefId = creDefCreatedResult.credentialDefinitionIds.get(creDefCreatedResult.credentialDefinitionIds.size() - 1);
 
         // Config Request
         V20PresSendRequestRequest presSendReqRequest = new V20PresSendRequestRequest();
+
         presSendReqRequest.comment = "string";
         presSendReqRequest.connectionId = connRecord.connectionId;
         presSendReqRequest.presentationRequest = new V20PresRequestByFormat();
         presSendReqRequest.presentationRequest.indy = new V20PresRequestByFormat.Indy();
-        presSendReqRequest.presentationRequest.indy.name = "Proof of ".concat(alias);
+        presSendReqRequest.presentationRequest.indy.name = "Proof of Hopae Pass";
+//        presSendReqRequest.presentationRequest.indy.nonRevoked = new V20PresRequestByFormat.Indy.NonRevoked();
+//        presSendReqRequest.presentationRequest.indy.nonRevoked.to = 1622833408;
         presSendReqRequest.presentationRequest.indy.version = "1.0";
         presSendReqRequest.presentationRequest.indy.requestedAttributes = new HashMap<>();
+
         List<Map<String, Object>> restrictions = new ArrayList<>();
         Map<String, Object> restriction = new HashMap<>();
+
         restriction.put("cred_def_id", creDefId);
         restrictions.add(restriction);
+
         presSendReqRequest.presentationRequest.indy.requestedAttributes.put(
-                "0_kakao_id_uuid", new IndyProofReqAttrSpec("kakao_id", null, null, restrictions));
+                "0_student_id_uuid", new IndyProofReqAttrSpec("student_id", null, null, restrictions));
+        presSendReqRequest.presentationRequest.indy.requestedAttributes.put(
+                "0_name_uuid", new IndyProofReqAttrSpec("name", null, null, restrictions));
+        presSendReqRequest.presentationRequest.indy.requestedAttributes.put(
+                "0_timestamp_uuid", new IndyProofReqAttrSpec("timestamp", null, null, restrictions));
         presSendReqRequest.presentationRequest.indy.requestedPredicates = new HashMap<>();
+        presSendReqRequest.presentationRequest.indy.requestedPredicates.put(
+                "0_temp_L_uuid", new IndyProofReqPredSpec("temp", null, "<", 375, restrictions));
 
         // Send proof request
         V20PresExRecord presExRecord = ariesRepository.requestProof(presSendReqRequest);
 
-        Map<String, Object> map = new HashMap<>();
-
-        map.put("state", presExRecord.state);
-        map.put("pres_ex_id", presExRecord.presExId);
-        map.put("thread_id", presExRecord.threadId);
-        map.put("updated_at", presExRecord.updatedAt);
-
-        return map;
+        return verify(presExRecord.presExId);
     }
 
     @RequestMapping(value = "/verified", method = RequestMethod.GET)
-    public Map<String, Object> verify(@RequestParam String pres_ex_id) throws Exception {
+    public Map<String, Object> verify(@RequestParam String presExId) throws Exception {
         Map<String, Object> map = new HashMap<>();
 
         // Verify Presentation
-        map.put("verified", ariesRepository.getPresentation(pres_ex_id).verified.equals("true"));
+        map.put("verified", ariesRepository.getPresentation(presExId).verified.equals("true"));
 
         return map;
     }
