a1f2409729b1556c22d89ba192b8a0ce291517ef
Novang <qkdxorjs1002@naver.com>
2021-06-06 23:41:42 +0900

    IssuerController: Hopae

--- src/main/java/kr/hopae/core/controller/IssuerController.java
+++ src/main/java/kr/hopae/core/controller/IssuerController.java
@@ -2,9 +2,12 @@ package kr.hopae.core.controller;
 
 import kr.hopae.aries.model.*;
 import kr.hopae.aries.repository.AriesRepository;
+import kr.hopae.baekseok.model.LoginInfo;
+import kr.hopae.baekseok.repository.BUAuthRepository;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import java.util.*;
 
 @RestController
@@ -14,95 +17,196 @@ public class IssuerController {
     @Resource(name = "ariesRepository")
     private AriesRepository ariesRepository;
 
-    @RequestMapping(value = "/agent", method = RequestMethod.GET)
-    public Map<String, Object> getAgent(@RequestParam String token) throws Exception {
+    @RequestMapping(value = "/create-session", method = RequestMethod.GET)
+    public Map<String, Object> requestAgent(HttpSession session,
+                                            @RequestParam int univerGu,
+                                            @RequestParam String userId,
+                                            @RequestParam String userPw) throws Exception {
+
+        // Initialize BUAuthRepository
+        BUAuthRepository buAuthRepository = new BUAuthRepository();
+
+        // Request Login
+        if (!buAuthRepository.requestLogin(univerGu, userId, userPw)) {
+            throw new Exception("Login failure");
+        }
+
+        // Request LoginInfo
+        LoginInfo loginInfo = buAuthRepository.requestLoginInfo();
+
+        if (!loginInfo.isLogined) {
+            throw new Exception("Login Info is not valid");
+        }
+
+        // Generate alias
+        String alias = "hopae-".concat(loginInfo.userId).concat("-").concat(UUID.randomUUID().toString());
+
+        // Set LoginInfo Attribute
+        session.setAttribute("LoginInfo", loginInfo);
+        session.setAttribute("Alias", alias);
 
         Map<String, Object> map = new HashMap<>();
 
-        map.put("port", 8031);
-        map.put("alias", "holder-".concat(UUID.randomUUID().toString()));
+        map.put("port", 1062);
+        map.put("alias", alias);
 
         return map;
     }
 
-    @RequestMapping(value = "/invitation", method = RequestMethod.GET)
-    public ConnectionInvitation requestInvitation(@RequestParam String token,
-                                                  @RequestParam String alias) throws Exception {
+    @RequestMapping(value = "/create-invitation", method = RequestMethod.GET)
+    public ConnectionInvitation requestCreateInvitation(HttpSession session) throws Exception {
+
+        // Check LoginInfo
+        LoginInfo loginInfo = (LoginInfo) session.getAttribute("LoginInfo");
+
+        if (!loginInfo.isLogined) {
+            throw new Exception("Login Info is not valid");
+        }
+
+        // Get Attributes from Session
+        String alias = (String) session.getAttribute("Alias");
 
         // Create Invitation
-        return ariesRepository.createInvitation(alias, true).invitation;
+        InvitationResult invitationResult = ariesRepository.createInvitation(alias, true);
+        session.setAttribute("ConnectionId", invitationResult.connectionId);
+
+        return invitationResult.invitation;
     }
 
-    @RequestMapping(value = "/credential", method = RequestMethod.GET)
-    public Map<String, Object> requestCredential(@RequestParam String token,
-                                             @RequestParam String alias) throws Exception {
+    @RequestMapping(value = "/receive-invitation", method = RequestMethod.POST)
+    public ConnRecord requestReceiveInvitation(HttpSession session,
+                                               @RequestBody ConnectionInvitation invitation) throws Exception {
 
-        // Get Connections
-        ConnectionList connectionList = ariesRepository.getConnections(alias);
-        ConnRecord connRecord = connectionList.results.get(0);
+        // Check LoginInfo
+        LoginInfo loginInfo = (LoginInfo) session.getAttribute("LoginInfo");
+
+        if (!loginInfo.isLogined) {
+            throw new Exception("Login Info is not valid");
+        }
+
+        // Get Attributes from Session
+        String alias = (String) session.getAttribute("Alias");
+
+        // Receive Invitation
+        ConnRecord connRecord = ariesRepository.receiveInvitation(alias, true, invitation);
+        session.setAttribute("ConnectionId", connRecord.connectionId);
+
+        return connRecord;
+    }
+
+    @RequestMapping(value = "/create-schema", method = RequestMethod.GET)
+    public Map<String, Object> requestCreateSchema(HttpSession session) throws Exception {
+
+        // Check LoginInfo
+        LoginInfo loginInfo = (LoginInfo) session.getAttribute("LoginInfo");
+
+        if (!loginInfo.isLogined) {
+            throw new Exception("Login Info is not valid");
+        }
 
         // Config Schema
         SchemaSendRequest schemaSendRequest = new SchemaSendRequest();
-        schemaSendRequest.schemaName = "account_schema";
+        schemaSendRequest.schemaName = "hopae_pass";
         schemaSendRequest.schemaVersion = "1.0";
         schemaSendRequest.attributes = new ArrayList<>();
-        schemaSendRequest.attributes.add("kakao_id");
-        schemaSendRequest.attributes.add("nickname");
-        schemaSendRequest.attributes.add("email");
-        schemaSendRequest.attributes.add("gender");
+        schemaSendRequest.attributes.add("student_id");
+        schemaSendRequest.attributes.add("name");
+        schemaSendRequest.attributes.add("temp");
         schemaSendRequest.attributes.add("timestamp");
 
         // Create Schema
-        TxnOrSchemaSendResult schemaSendResult = ariesRepository.createSchemas(connRecord.connectionId, schemaSendRequest);
+        TxnOrSchemaSendResult schemaSendResult = ariesRepository.createSchemas(null, schemaSendRequest);
+        session.setAttribute("SchemaId", schemaSendResult.sent.schemaId);
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("schema_id", schemaSendResult.sent.schemaId);
+
+        return map;
+    }
+
+    @RequestMapping(value = "/create-credential-def", method = RequestMethod.GET)
+    public Map<String, Object> requestCreateCredentialDef(HttpSession session) throws Exception {
+
+        // Check LoginInfo
+        LoginInfo loginInfo = (LoginInfo) session.getAttribute("LoginInfo");
+
+        if (!loginInfo.isLogined) {
+            throw new Exception("Login Info is not valid");
+        }
+
+        // Get Attributes from Session
+        String schemaId = (String) session.getAttribute("SchemaId");
 
         // Config Credential definition
         CredentialDefinitionSendRequest creDefSendRequest = new CredentialDefinitionSendRequest();
         creDefSendRequest.revocationRegistrySize = 1000;
-        creDefSendRequest.schemaId = schemaSendResult.sent.schemaId;
-        creDefSendRequest.tag = alias;
+        creDefSendRequest.schemaId = schemaId;
+        creDefSendRequest.supportRevocation = true;
+        creDefSendRequest.tag = "revocable";
 
         // Define Credential definition
-        TxnOrCredentialDefinitionSendResult creDefSendResult = ariesRepository.createCredentialDef(connRecord.connectionId, creDefSendRequest);
+        TxnOrCredentialDefinitionSendResult creDefSendResult = ariesRepository.createCredentialDef(null, creDefSendRequest);
+        session.setAttribute("CredDefId", creDefSendResult.sent.credentialDefinitionId);
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("cred_def_id", creDefSendResult.sent.credentialDefinitionId);
+
+        return map;
+    }
+
+    @RequestMapping(value = "/issue-credential", method = RequestMethod.GET)
+    public Map<String, Object> requestIssueCredential(HttpSession session,
+                                                      @RequestParam String temp) throws Exception {
+
+        // Check LoginInfo
+        LoginInfo loginInfo = (LoginInfo) session.getAttribute("LoginInfo");
+
+        if (!loginInfo.isLogined) {
+            throw new Exception("Login Info is not valid");
+        }
+
+        // Get Attributes from Session
+        String connectionId = (String) session.getAttribute("ConnectionId");
+        String schemaId = (String) session.getAttribute("SchemaId");
+        String credDefId = (String) session.getAttribute("CredDefId");
 
         // Config Issue Credential
         V20CredProposalRequestPreviewMand crePropRequest = new V20CredProposalRequestPreviewMand();
         crePropRequest.autoRemove = true;
         crePropRequest.comment = "string";
-        crePropRequest.connectionId = connRecord.connectionId;
+        crePropRequest.connectionId = connectionId;
         crePropRequest.credentialPreview = new V20CredPreview();
         crePropRequest.credentialPreview.type = "issue-credential/2.0/credential-preview";
         crePropRequest.credentialPreview.attributes = new ArrayList<>();
-/*        crePropRequest.credentialPreview.attributes.add(new V20CredAttrSpec(
-                null, "kakao_id", String.valueOf(userInfo.id)));
         crePropRequest.credentialPreview.attributes.add(new V20CredAttrSpec(
-                null, "nickname", userInfo.kakaoAccount.profile.nickname));
+                null, "student_id", loginInfo.userId));
         crePropRequest.credentialPreview.attributes.add(new V20CredAttrSpec(
-                null, "email", userInfo.kakaoAccount.isEmailValid ? userInfo.kakaoAccount.email : ""));
+                null, "name", loginInfo.userName));
         crePropRequest.credentialPreview.attributes.add(new V20CredAttrSpec(
-                null, "gender", userInfo.kakaoAccount.isEmailValid ? userInfo.kakaoAccount.gender : ""));
+                null, "temp", temp));
         crePropRequest.credentialPreview.attributes.add(new V20CredAttrSpec(
-                null, "timestamp", String.valueOf(new Date().getTime())));*/
+                null, "timestamp", String.valueOf(new Date().toInstant().getEpochSecond())));
         crePropRequest.filter = new V20CredProposalRequestPreviewMand.Filter();
-        crePropRequest.filter.dif = new V20CredProposalRequestPreviewMand.Filter.DIF();
-        crePropRequest.filter.dif.someDIFCriterion = "string";
         crePropRequest.filter.indy = new V20CredProposalRequestPreviewMand.Filter.Indy();
-        crePropRequest.filter.indy.credDefId = creDefSendResult.sent.credentialDefinitionId;
-        crePropRequest.filter.indy.issuerDID = creDefSendResult.sent.credentialDefinitionId.substring(
-                0, creDefSendResult.sent.credentialDefinitionId.indexOf(":"));
-        crePropRequest.filter.indy.schemaId = schemaSendResult.sent.schemaId;
+        crePropRequest.filter.indy.credDefId = credDefId;
+        crePropRequest.filter.indy.issuerDID = credDefId.split(":")[0];
+        crePropRequest.filter.indy.schemaId = schemaId;
         crePropRequest.filter.indy.schemaIssuerDID = crePropRequest.filter.indy.issuerDID;
-        crePropRequest.filter.indy.schemaName = schemaSendRequest.schemaName;
-        crePropRequest.filter.indy.schemaVersion = schemaSendRequest.schemaVersion;
-        crePropRequest.trace = true;
+        crePropRequest.filter.indy.schemaName = schemaId.split(":")[2];
+        crePropRequest.filter.indy.schemaVersion = schemaId.split(":")[3];
+        crePropRequest.trace = false;
 
         // Send credential
         V20CredExRecord credExRecord = ariesRepository.issueCredential(crePropRequest);
+        session.setAttribute("CredExId", credExRecord.credExId);
 
         Map<String, Object> map = new HashMap<>();
 
-        map.put("state", credExRecord.state);
+        map.put("cred_ex_id", credExRecord.credExId);
         map.put("thread_id", credExRecord.threadId);
-        map.put("updated_at", credExRecord.updatedAt);
+        map.put("created_at", credExRecord.updatedAt);
 
         return map;
     }
