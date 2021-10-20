6d6d0c71a667717677662a12cb1b8e9b1623d43a
paragonnov <qkdxorjs1002@naver.com>
2021-10-20 20:59:10 +0900

    Update batch

--- start_was.sh
+++ start_was.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#docker build --tag tomcat:9.0.46_1 ./docker
-docker run --rm -itd --name tomcat_was -v /etc/localtime:/etc/localtime --net=host -p 1060:8080 tomcat:hopae
+docker pull qkdxorjs1002/tomcat:hopae
+docker run --rm -itd --name tomcat_was -v /etc/localtime:/etc/localtime --net=host -p 1060:8080 qkdxorjs1002/tomcat:hopae
